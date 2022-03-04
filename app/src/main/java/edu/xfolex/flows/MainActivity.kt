cfeb9b756684978a86ab9399b38c79afaef2d824
Oleksandr Filonenko <filonenko.oleksa@gmail.com>
2022-03-04 13:02:57 +0200

    The basic example of flows

---
+++ app/src/main/java/edu/xfolex/flows/MainActivity.kt
@@ -0,0 +1,41 @@
+package edu.xfolex.flows
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import edu.xfolex.flows.ui.theme.FlowsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlowsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val viewModel = viewModel<MainViewModel>()
+                    val time = viewModel.countDownFlow.collectAsState(initial = 100)
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = time.value.toString(),
+                            fontSize = 30.sp,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/edu/xfolex/flows/MainViewModel.kt
@@ -0,0 +1,34 @@
+package edu.xfolex.flows
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    val countDownFlow = flow<Int> {
+        val startValue = 100
+        var currentValue = startValue
+        emit(startValue)
+        while (currentValue > 0) {
+            delay(1000L)
+            currentValue--
+            emit(currentValue)
+        }
+    }
+
+    init {
+        collectFlow()
+    }
+
+    private fun collectFlow() {
+        viewModelScope.launch {
+            countDownFlow.collect { time ->
+                println("The current time is: $time")
+            }
+        }
+    }
+}
